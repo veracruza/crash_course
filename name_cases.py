0abe17d47a403b8a637bb94354808fcbdc7c5c7f
Veronika Luhova <nikanika1909@gmail.com>
2022-12-22 23:39:27 +0200

    + 2-5, 2-6

--- name_cases.py
+++ name_cases.py
@@ -15,3 +15,10 @@ print(name.upper())
 
 #2-5.Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
 
+print ("Volodymyr Zelensky once said: 'Light will win over darkness.''")
+
+#2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and represent it with a new variable called message. Print your message.
+
+famous_person = 'Volodymyr Zelensky'
+message = "'I need ammunition, not a ride.'"
+print("{0} once said: {1}".format(famous_person, message))
