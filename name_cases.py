dc6e25f2ec01d5ea59a9f272a03bfc4030436703
Veronika Luhova <nikanika1909@gmail.com>
2022-12-22 05:14:47 +0200

    22.12.22 start

---
+++ name_cases.py
@@ -0,0 +1,8 @@
+#2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+
+name = "Veronica"
+message = ", what is your favorite programming language?"
+message_for = f"{name}{message}"
+print(message_for)
+
+#2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
