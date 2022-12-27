547b706c4815deb58ea30d49bf22f988972a895f
Veronika Luhova <nikanika1909@gmail.com>
2022-12-27 23:33:29 +0200

    + 3-1 Names

---
+++ 3_lists.py
@@ -0,0 +1,10 @@
+#3-1. Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+
+friends = ['Kate', 'Alex', 'Victoria']
+print(friends[0].title())
+print(friends[1].title())
+print(friends[2].title())
+
+
+#3-2: Greetings
+#Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
