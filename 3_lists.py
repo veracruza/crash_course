52b9a10e1299f895e94fdc3521ad64b06c3138c7
Veronika Luhova <nikanika1909@gmail.com>
2022-12-29 20:30:21 +0200

    + 3-7

--- 3_lists.py
+++ 3_lists.py
@@ -82,5 +82,31 @@ print(f"{name}, please come to dinner.")
 name = guests[5].title()
 print(f"{name}, please come to dinner.")
 
+#3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
 
+print("\nSorry, we can only invite two people to dinner.")
 
+#Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+eva_off = guests.pop(0)
+print(f"Sorry, {eva_off.title()} there's no room at the table.")
+
+sophia_off = guests.pop(2)
+print(f"Sorry, {sophia_off.title()} there's no room at the table.")
+
+melanie_off = guests.pop(3)
+print(f"Sorry, {melanie_off.title()} there's no room at the table.")
+
+victoria_off = guests.pop(2)
+print(f"Sorry, {victoria_off.title()} there's no room at the table.")
+
+#Print a message to each of the two people still on your list, letting them know they’re still invited.
+name = guests[0].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+#Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+del(guests[0])
+del(guests[0])
+print(guests)
