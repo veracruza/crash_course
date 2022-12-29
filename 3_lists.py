87cfb1385b9cf3bccb27b187851df20a64490d30
Veronika Luhova <nikanika1909@gmail.com>
2022-12-29 21:00:03 +0200

    + 3-8

--- 3_lists.py
+++ 3_lists.py
@@ -110,3 +110,39 @@ print(f"{name}, please come to dinner.")
 del(guests[0])
 del(guests[0])
 print(guests)
+
+#3-8. Seeing the World: Think of at least five places in the world you’d like to visit.
+#Store the locations in a list. Make sure the list is not in alphabetical order.
+#Print your list in its original order. Don’t worry about printing the list neatly, just print it as a raw Python list.
+locations = ['berlin', 'nederland', 'alps', 'paris', 'alicante']
+print("\nHere is the original list:")
+print(locations)
+
+#Use sorted() to print your list in alphabetical order without modifying the actual list.
+#Show that your list is still in its original order by printing it.
+print("\nHere is the sorted list:")
+print(sorted(locations))
+
+#Use sorted() to print your list in reverse alphabetical order without chang- ing the order of the original list.
+print("\nReverse alphabetical:")
+print(sorted(locations, reverse=True))
+
+#Use reverse() to change the order of your list. Print the list to show that its order has changed
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+#Use reverse() to change the order of your list again. Print the list to show it’s back to its original order.
+print("\nOriginal order:")
+locations.reverse()
+print(locations)
+
+#Use sort() to change your list so it’s stored in alphabetical order. Print the list to show that its order has been changed.
+print("\nAlphabetical")
+locations.sort()
+print(locations)
+
+#Use sort() to change your list so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+print("\nReverse alphabetical")
+locations.sort(reverse=True)
+print(locations)
