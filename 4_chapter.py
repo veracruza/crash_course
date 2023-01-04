3abf39221388ee926c5c746652063da203712ef2
Veronika Luhova <nikanika1909@gmail.com>
2023-01-05 00:52:58 +0200

    + 4-3, 4-5, 4-6, 4-7, 4-8, 4-9

--- 4_chapter.py
+++ 4_chapter.py
@@ -21,3 +21,49 @@ for pet in favorite_pets:
     print(f"A {pet} would make a great pet")
 #Add a line at the end of your program stating what these animals have in common.
 print("\nAny of these animals would make a great pet!")
+
+#4-3. Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
+
+numbers = list(range(1,21))
+for number in numbers:
+    print(number)
+
+#4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. Also, use the sum() function to see how quickly Python can add a million numbers.
+
+numbers = list(range(1, 1_000_001))
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+#4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
+
+odd_numbers = list(range(1, 21, 2))
+for number in odd_numbers:
+    print(number)
+
+#4-7. Threes: Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.
+
+threes = list(range(3, 31, 3))
+for number in threes:
+    print(number)
+
+#4-8. Cubes: A number raised to the third power is called a cube. For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
+
+cubes = []
+for number in range(1, 11):
+    cube = number**3
+    cubes.append(cube)
+for cube in cubes:
+    print(cube)
+
+#4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
+
+cubes = [number**3 for number in range(1, 11)]
+for cube in cubes:
+    print(cube)
+
+
+
+
+
