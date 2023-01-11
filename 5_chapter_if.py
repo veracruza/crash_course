f6d4544259172b248b97c1f14ae335de80d513d5
Veronika Luhova <nikanika1909@gmail.com>
2023-01-11 23:39:35 +0200

    + 5-6, 5-7

--- 5_chapter_if.py
+++ 5_chapter_if.py
@@ -49,3 +49,43 @@ elif alien_color == 'yellow':
 else:
     print("You just earned 15 points!")
 
+#5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+#If the person is less than 2 years old, print a message that the person is a baby.
+#If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+#If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+#If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+#If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+#If the person is age 65 or older, print a message that the person is an elder.
+
+age = 19
+
+if age < 2:
+    print("You're a baby!")
+elif age < 4:
+    print("You're a toddler!")
+elif age < 13:
+    print("You're a kid!")
+elif age < 20:
+    print("You're a teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
+
+#5-7. Favorite Fruit: Make a list of your favorite fruits,
+# and then write a series of independent if statements that check for certain fruits in your list.
+#Make a list of your three favorite fruits and call it favorite_fruits.
+#Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!
+
+favorite_fruits = ['bananas', 'kiwis', 'strawberry']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'peaches' in favorite_fruits:
+    print("You really like peaches!")
