2b4f961a68dac08416cf2132011224f8699c139e
Veronika Luhova <nikanika1909@gmail.com>
2023-01-11 23:14:53 +0200

    - 4-15

--- 4_chapter.py
+++ 4_chapter.py
@@ -135,11 +135,7 @@ print("You can now choose from the following items:")
 for item in menu_items:
     print(f"- {item}")
 
-#4-15. Code Review: Choose three of the programs you’ve written in this chapter and modify each one to comply with PEP 8:
 
-#Use four spaces for each indentation level. Set your text editor to insert four spaces every time you press tab, if you haven’t already done so (see Appendix B for instructions on how to do this).
-#Use less than 80 characters on each line, and set your editor to show a vertical guideline at the 80th character position.
-#Don’t use blank lines excessively in your program files.
 
 
 
