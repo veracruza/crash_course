ba112acdab09adc4cc1e6d3342cb6befbcd5091b
Veronika Luhova <nikanika1909@gmail.com>
2023-01-11 23:32:59 +0200

    + 5-4, 5-5

--- 5_chapter_if.py
+++ 5_chapter_if.py
@@ -15,4 +15,37 @@ alien_color = 'red'
 if alien_color == 'green':
     print("You just earned 5 points!")
 
+#5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+#If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+#If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+#Write one version of this program that runs the if block and another that runs the else block.
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+#5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif- else chain.
+#If the alien is green, print a message that the player earned 5 points.
+#If the alien is yellow, print a message that the player earned 10 points.
+#If the alien is red, print a message that the player earned 15 points.
+#Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
 
