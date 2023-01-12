667ffe37b528956355af269504887708693b1a52
Veronika Luhova <nikanika1909@gmail.com>
2023-01-12 21:37:44 +0200

    + 5-8, 5-9

--- 5_chapter_if.py
+++ 5_chapter_if.py
@@ -1,9 +1,9 @@
 #5-3. Alien Colors #1: Imagine an alien was just shot down in a game.
-#Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-#Write an if statement to test whether the alien’s color is green.
-# If it is, print a message that the player just earned 5 points.
-#Write one version of this program that passes the if test and another that fails.
-# (The version that fails will have no output.)
+# - Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# - Write an if statement to test whether the alien’s color is green.
+# - If it is, print a message that the player just earned 5 points.
+# - Write one version of this program that passes the if test and another that fails.
+# - (The version that fails will have no output.)
 
 alien_color = 'green'
 
@@ -16,8 +16,8 @@ if alien_color == 'green':
     print("You just earned 5 points!")
 
 #5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
-#If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
-#If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+# - If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+# - If the alien’s color isn’t green, print a statement that the player just earned 10 points.
 
 alien_color = 'green'
 
@@ -26,7 +26,7 @@ if alien_color == 'green':
 else:
     print("You just earned 10 points!")
 
-#Write one version of this program that runs the if block and another that runs the else block.
+# - Write one version of this program that runs the if block and another that runs the else block.
 alien_color = 'yellow'
 
 if alien_color == 'green':
@@ -35,10 +35,10 @@ else:
     print("You just earned 10 points!")
 
 #5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif- else chain.
-#If the alien is green, print a message that the player earned 5 points.
-#If the alien is yellow, print a message that the player earned 10 points.
-#If the alien is red, print a message that the player earned 15 points.
-#Write three versions of this program, making sure each message is printed for the appropriate color alien.
+# - If the alien is green, print a message that the player earned 5 points.
+# - If the alien is yellow, print a message that the player earned 10 points.
+# - If the alien is red, print a message that the player earned 15 points.
+# - Write three versions of this program, making sure each message is printed for the appropriate color alien.
 
 alien_color = 'red'
 
@@ -49,13 +49,14 @@ elif alien_color == 'yellow':
 else:
     print("You just earned 15 points!")
 
-#5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
-#If the person is less than 2 years old, print a message that the person is a baby.
-#If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
-#If the person is at least 4 years old but less than 13, print a message that the person is a kid.
-#If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
-#If the person is at least 20 years old but less than 65, print a message that the person is an adult.
-#If the person is age 65 or older, print a message that the person is an elder.
+#5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life.
+# Set a value for the variable age, and then:
+# - If the person is less than 2 years old, print a message that the person is a baby.
+# - If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+# - If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+# - If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+# - If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+# - If the person is age 65 or older, print a message that the person is an elder.
 
 age = 19
 
@@ -74,8 +75,8 @@ else:
 
 #5-7. Favorite Fruit: Make a list of your favorite fruits,
 # and then write a series of independent if statements that check for certain fruits in your list.
-#Make a list of your three favorite fruits and call it favorite_fruits.
-#Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!
+# - Make a list of your three favorite fruits and call it favorite_fruits.
+# - Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!
 
 favorite_fruits = ['bananas', 'kiwis', 'strawberry']
 
@@ -89,3 +90,32 @@ if 'kiwis' in favorite_fruits:
     print("You really like kiwis!")
 if 'peaches' in favorite_fruits:
     print("You really like peaches!")
+
+#5-8. Hello Admin: Make a list of five or more usernames, including the name
+#'admin'. Imagine you are writing code that will print a greeting to each user
+#after they log in to a website. Loop through the list, and print a greeting to each user:
+# - If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+# - Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.
+
+usernames = ['eva', 'kate', 'admin', 'victoria', 'alex']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for loggin in again!")
+
+#5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+# - If the list is empty, print the message We need to find some users!
+# - Remove all of the usernames from your list, and make sure the correct message is printed.
+
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {username}, thank you for loggin in again!")
+else:
+    print("We need to find some users!")
