f8ae17c5cf52989ac70f604ebaceb6f95974bae4
Veronika Luhova <nikanika1909@gmail.com>
2023-01-16 20:25:53 +0200

    + 6-2

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -15,3 +15,31 @@ print(person['last_name'])
 print(person['age'])
 print(person['city'])
 
+#6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+# Think of five names, and use them as keys in your dictionary. Think of a favorite
+# number for each person, and store each as a value in your dictionary.
+# Print each person’s name and their favorite number. For even more fun, poll a few friends
+# and get some actual data for your program.
+
+favorite_numbers = {
+    'kate': 13,
+    'eva': 23,
+    'alex': 77,
+    'vera': 19,
+    'maxim': 6,
+    }
+
+num = favorite_numbers['kate']
+print(f"Kate's favorite number is {num}.")
+
+num = favorite_numbers['eva']
+print(f"Eva's favorite number is {num}.")
+
+num = favorite_numbers['alex']
+print(f"Alex's favorite number is {num}.")
+
+num = favorite_numbers['vera']
+print(f"Vera's favorite number is {num}.")
+
+num = favorite_numbers['maxim']
+print(f"Maxim's favorite number is {num}.")
