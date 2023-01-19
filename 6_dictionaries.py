a42c43430083d92ae6ddf59740305379578f7096
Veronika Luhova <nikanika1909@gmail.com>
2023-01-19 23:32:21 +0200

    + 6-3

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -51,3 +51,26 @@ print(f"Maxim's favorite number is {num}.")
 #Print each word and its meaning as neatly formatted output. You might print the word followed by
 # a colon and then its meaning, or print the word on one line and then print its meaning indented on
 # a second line. Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+
+glossary = {
+    'string': 'A series of characters.',
+    'comment': 'A note in a program that the Python interpreter ignores.',
+    'list': 'A collection of items in a particular order.',
+    'loop': 'Work through a collection of items, one at a time.',
+    'dictionary': "A collection of key-value pairs.",
+    }
+
+word = 'string'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'comment'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'list'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'loop'
+print(f"\n{word.title()}: {glossary[word]}")
+
+word = 'dictionary'
+print(f"\n{word.title()}: {glossary[word]}")
