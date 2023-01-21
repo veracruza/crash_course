16fb1c37eb1b243548a0daf6b2e864c87f16acfe
Veronika Luhova <nikanika1909@gmail.com>
2023-01-21 21:11:17 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -74,3 +74,9 @@ print(f"\n{word.title()}: {glossary[word]}")
 
 word = 'dictionary'
 print(f"\n{word.title()}: {glossary[word]}")
+
+#6-4: Glossary 2  Now that you know how to loop through a dictionary, clean
+# up the code from Exercise 6-3 (page 99) by replacing your series of print()
+# calls with a loop that runs through the dictionary’s keys and values.
+# When you’re sure that your loop works, add five more Python terms to your glossary.
+# When you run your program again, these new words and meanings should automatically be included in the output.
