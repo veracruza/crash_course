17da3ac8f11225f4114228d8790e446097b2f353
Veronika Luhova <nikanika1909@gmail.com>
2023-01-28 23:28:24 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -100,6 +100,7 @@ for word, definition in glossary.items():
 #6-5. Rivers: Make a dictionary containing three major rivers and the country each river runs through.
 # One key-value pair might be 'nile': 'egypt'
 #Use a loop to print a sentence about each river, such as The Nile runs through Egypt.
-
+#Use a loop to print the name of each river included in the dictionary.
+#Use a loop to print the name of each country included in the dictionary.
 
 
