8625504d122b8f0a603b6056f267f98e6ec6a0e4
Veronika Luhova <nikanika1909@gmail.com>
2023-02-06 23:45:47 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -113,3 +113,7 @@ rivers = {
 
 for river, country in rivers.items():
     print(f"The {river.title()} flows through {country.title()}.")
+
+print("\nThe following rivers are included in this data set:")
+for river in rivers.keys():
+    print(f"- {river.title()}")
