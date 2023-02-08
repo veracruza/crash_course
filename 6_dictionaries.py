7dbc7d750ea06da4be3b8878c85be60463f9b2e2
Veronika Luhova <nikanika1909@gmail.com>
2023-02-08 23:38:39 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -117,3 +117,7 @@ for river, country in rivers.items():
 print("\nThe following rivers are included in this data set:")
 for river in rivers.keys():
     print(f"- {river.title()}")
+
+print("\nThe following countries are included in this data set:")
+for country in rivers.values():
+    print(f"- {country.title()}")
