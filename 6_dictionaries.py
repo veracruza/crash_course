ea6128b7b23087a783c6cf087eabe5af8088e995
Veronika Luhova <nikanika1909@gmail.com>
2023-02-13 22:43:10 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -125,4 +125,7 @@ for country in rivers.values():
 #6-6: Polling
 #Make a list of people who should take the favorite languages poll. Include
 #some names that are already in the dictionary and some that are not.
+#Loop through the list of people who should take the poll. If they have already taken the poll,
+# print a message thanking them for responding. If they have not yet taken the poll,
+# print a message inviting them to take the poll.
 
