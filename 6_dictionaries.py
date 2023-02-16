c2348f4021878fa0fbb8328e757b1474eea43115
Veronika Luhova <nikanika1909@gmail.com>
2023-02-16 23:37:39 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -135,3 +135,5 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python',
     }
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
