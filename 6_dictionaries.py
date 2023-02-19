9cd4f36fcc37ff9a49c11b0017f151671fa9c3d8
Veronika Luhova <nikanika1909@gmail.com>
2023-02-19 23:20:14 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -143,3 +143,5 @@ for name, language in favorite_languages.items():
     for coder in coders:
         if coder in favorite_languages.keys():
             print(f"Thank you for taking the poll, {coder.title()}!")
+        else:
+            print(f"{coder.title()}, what's your favorite programming language?")
