d6a0ca24a8735fc4f99a3bc9350b20cda917e502
Veronika Luhova <nikanika1909@gmail.com>
2023-02-21 21:37:56 +0200

    +

--- 6_dictionaries.py
+++ 6_dictionaries.py
@@ -147,3 +147,6 @@ for name, language in favorite_languages.items():
             print(f"{coder.title()}, what's your favorite programming language?")
 
 #6-7: People
+#Start with the program you wrote for Exercise 6-1 (page 102). Make two new dictionaries representing different people,
+# and store all three dictionaries in a list called people. Loop through your list of people.
+# As you loop through the list, print everything you know about each person.
